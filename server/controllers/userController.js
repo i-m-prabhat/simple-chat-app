de25569747bb47808c55d8e79d415f86fbababf6
Prabhat <117756490+i-m-prabhat@users.noreply.github.com>
2024-11-20 23:58:53 +0530

    get logged user data api added

---
+++ server/controllers/userController.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const User = require('../model/user');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+
+router.get('/get-logged-user', authMiddleware, async (req, res) =>
+{
+    try
+    {
+        const user = await User.findById(req.user._id).select('-password');
+        return res.send({
+            status: true,
+            message: "Get User Details Successfully!",
+            user: user
+        });
+    }
+    catch (err)
+    {
+        return res.send({
+            status: false,
+            message: err.message
+        })
+    }
+})
+
+
+module.exports = router;
