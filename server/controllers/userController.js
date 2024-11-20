bb823ae3ed01230d4a17bb793f7f5834d97e9a4c
Prabhat <117756490+i-m-prabhat@users.noreply.github.com>
2024-11-21 00:01:59 +0530

    get all users api created

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -21,7 +21,28 @@ router.get('/get-logged-user', authMiddleware, async (req, res) =>
             message: err.message
         })
     }
-})
+});
+
+
+router.get('/get-all-users', authMiddleware, async (req, res) =>
+{
+    try
+    {
+        const user = await User.find({ _id: { $ne: req.user._id } }).select('-password');
+        return res.send({
+            status: true,
+            message: "Get Users Details Successfully!",
+            user: user
+        });
+    }
+    catch (err)
+    {
+        return res.send({
+            status: false,
+            message: err.message
+        })
+    }
+});
 
 
 module.exports = router;
